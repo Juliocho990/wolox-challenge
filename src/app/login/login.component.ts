23ad54d8527697e5c016af37b53505bacc1e957a
julius <loperabarrientos@gmail.com>
2020-08-27 12:44:23 -0500

    login layout, reactive forms, validators, guards and navigation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { UserCredential } from '../user-credential';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  public isSubmitted = false;
+
+  constructor(private auth: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      keepSession: [false]
+    })
   }
+
+  login() {
+    this.isSubmitted = true;
+    if(this.loginForm.invalid) {
+      return;
+    }
+    if (this.loginForm.get('keepSession').value) {
+      if (confirm('¿Deseas conservar los datos de sesión?')) {
+        this.auth.login(<UserCredential>this.loginForm.value);
+      } 
+    } else {
+      this.auth.login(<UserCredential>this.loginForm.value);
+    }
+  }
+
+  get formControls() { return this.loginForm.controls; }
 
 }
